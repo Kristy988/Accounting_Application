4e0d3f1268068d3c145fe7bf9d909024d79240dd
KRISTY\kristy <988kristy@gmail.com>
2026-05-28 18:01:04 +0800

    帳務分析-使用反射完成篩選2

--- 記帳APP/Presenter/AnaylzePresneter.cs
+++ 記帳APP/Presenter/AnaylzePresneter.cs
@@ -28,59 +28,47 @@ namespace 記帳APP.Presenter
             List<RecordDAO> recordDAOs = recordRepository.GetRecords(fromDate, toDate);
             IEnumerable<AnalyzeDTO> analyzeDTOs = Mapper.Map<RecordDAO, AnalyzeDTO>(recordDAOs);
             //篩選有DisplayName的屬性
-            var attibuteAfterFilter = typeof(AnalyzeDTO).GetProperties().Where(x => x.GetCustomAttribute<DisplayNameAttribute>() != null).ToList();
+            var attributeAfterFilter = typeof(AnalyzeDTO).GetProperties().Where(x => x.GetCustomAttribute<DisplayNameAttribute>() != null).ToList();
             List<AnalyzeDTO> newList = new List<AnalyzeDTO>();
 
             if (filters.Count != 0)
             {
-                var filterList = new List<PropertyInfo>();
-
                 newList = analyzeDTOs.Where(x =>
                 {
-                    bool rawDataAfterFilter = attibuteAfterFilter.Any(y =>
-                    {
-                        //比對屬性是否存在於filter
-                        if (!filters.ContainsKey(y.GetCustomAttribute<DisplayNameAttribute>().DisplayName))
-                            return false;
-                        var prop = filters.FirstOrDefault(z => z.Key == y.GetCustomAttribute<DisplayNameAttribute>().DisplayName);
+                    List<bool> bools = new List<bool>();
+                    int filterCount = 0;
+                    if (filters.ContainsKey("方式"))
+                        filterCount++;
+                    if (filters.ContainsKey("對象"))
+                        filterCount++;
+                    //如果有目的 則filterCount+1
+                    bool checkIfSub = filters.Any(z => z.Key.Contains("目的"));
+                    if (checkIfSub)
+                        filterCount++;
 
-                        //比對rawData是否符合filter要求
-                        return filters[prop.Key].Contains(y.GetValue(x));
+                    attributeAfterFilter.ForEach(y =>
+                    {
+                        string title = y.GetCustomAttribute<DisplayNameAttribute>().DisplayName;
+                        
+                        if (title.Contains("目的"))
+                            title = y.GetCustomAttribute<DisplayNameAttribute>().DisplayName + x.Category;
+                        
+                        //比對屬性是否存在於filter,且欄位不是類型
+                        if (filters.ContainsKey(title) && title != "類型")
+                        {
+                            var prop = filters.FirstOrDefault(z => z.Key == title);
+                            //比對rawData是否符合filter要求
+                            bools.Add(filters[prop.Key].Contains(y.GetValue(x)));
+                        }
                     });
-                    return rawDataAfterFilter;
+
+                    //符合所有篩選條件 則回傳true 反之則false
+                    return bools.All(boolean => boolean) && bools.Count() == filterCount;
                 }).ToList();
             }
             else
                 newList = analyzeDTOs.ToList();
 
-
-            //if (filters.Count != 0)
-            //{
-            //    newList = analyzeDTOs.Where(x =>
-            //    {
-            //        int filterCount = filters.Count;
-            //        List<bool> bools = new List<bool>();
-            //        if (filters.Keys.Contains("對象"))
-            //        {
-            //            bools.Add(filters["對象"].Contains(x.Target));
-            //        }
-            //        if (filters.Keys.Contains("方式"))
-            //        {
-            //            bools.Add(filters["方式"].Contains(x.Payment));
-            //        }
-            //        string key = $"目的{x.Category}";
-
-            //        if (filters.ContainsKey(key))
-            //        {
-            //            bools.Add(filters[key].Contains(x.Subcategory));
-            //        }
-            //        if (filters.Keys.Contains("類型"))
-            //            filterCount--;
-            //        return bools.All(boolean => boolean) && bools.Count() == filterCount;
-            //    }).ToList();
-            //}
-            //else
-            //    newList = analyzeDTOs.ToList();
 
             var con1 = groups.First();
             var con2 = groups.Last();
